c7cabe21376781bb3be030769e78908ca9a0d888
AdeshDaniel <stockhutfxcompany@gmail.com>
2023-03-11 04:10:53 +0100

    made changes to the navbar, ecommerce page halfway

--- src/App.js
+++ src/App.js
@@ -61,33 +61,34 @@ const App = () => {
             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
               <Navbar />
             </div>
+
+            <Routes>
+              {/* Dashboard Links */}
+              <Route path='/' element={<Ecommerce />} />
+              <Route path='/ecommerce' element={<Ecommerce />} />
+
+              {/* Pages */}
+              <Route path='/orders' element={<Orders />} />
+              <Route path='/employees' element={<Employees />} />
+              <Route path='/cutomers' element={<Customers />} />
+
+              {/* App routes */}
+              <Route path='/kanban' element={<Kanban />} />
+              <Route path='/editor' element={<Editor />} />
+              <Route path='/calender' element={<Calendar />} />
+              <Route path='/color-picker' element={<ColorPicker />} />
+
+              {/* charts */}
+              <Route path='/line' element={<Line />} />
+              <Route path='/area' element={<Area />} />
+              <Route path='/bar' element={<Bar />} />
+              <Route path='/pie' element={<Pie />} />
+              <Route path='/financial' element={<Financial />} />
+              <Route path='/color-mapping' element={<ColorMapping />} />
+              <Route path='/pyramid' element={<Pyramid />} />
+              <Route path='/stacked' element={<Stacked />} />
+            </Routes>
           </div>
-          <Routes>
-            {/* Dashboard Links */}
-            <Route path='/' element={<Ecommerce />} />
-            <Route path='/ecommerce' element={<Ecommerce />} />
-
-            {/* Pages */}
-            <Route path='/orders' element={<Orders />} />
-            <Route path='/employees' element={<Employees />} />
-            <Route path='/cutomers' element={<Customers />} />
-
-            {/* App routes */}
-            <Route path='/kanban' element={<Kanban />} />
-            <Route path='/editor' element={<Editor />} />
-            <Route path='/calender' element={<Calendar />} />
-            <Route path='/color-picker' element={<ColorPicker />} />
-
-            {/* charts */}
-            <Route path='/line' element={<Line />} />
-            <Route path='/area' element={<Area />} />
-            <Route path='/bar' element={<Bar />} />
-            <Route path='/pie' element={<Pie />} />
-            <Route path='/financial' element={<Financial />} />
-            <Route path='/color-mapping' element={<ColorMapping />} />
-            <Route path='/pyramid' element={<Pyramid />} />
-            <Route path='/stacked' element={<Stacked />} />
-          </Routes>
         </div>
       </BrowserRouter>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,13 @@ import { useStateContext } from "../contexts/ContextProvider";
 import { links } from "../data/dummy";
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSideBar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
 
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
@@ -23,7 +29,7 @@ const Sidebar = () => {
           <div className='flex justify-between items-center'>
             <Link
               to='/'
-              onClick={() => setActiveMenu(false)}
+              onClick={handleCloseSideBar}
               className='items-center mt-4 ml-3 gap-3 flex font-extrabold tracking-tight dark:txt-white text-slate-900'
             >
               <SiShopware className='text-3xl ' />
@@ -49,7 +55,7 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={() => {}}
+                    onClick={handleCloseSideBar}
                     className={({ isAcitve }) =>
                       isAcitve ? activeLink : normalLink
                     }
