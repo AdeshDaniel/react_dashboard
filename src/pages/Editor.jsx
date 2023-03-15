7421221fd6f5ed204a975763e20129de11317c47
AdeshDaniel <stockhutfxcompany@gmail.com>
2023-03-15 18:40:45 +0100

    finished with the pages

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import {
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  RichTextEditorComponent,
+  Toolbar,
+  QuickToolbar,
+} from "@syncfusion/ej2-react-richtexteditor";
+import { Header } from "../components";
+import { EditorData } from "../data/dummy";
 
 const Editor = () => {
-  return <div>Editor</div>;
+  return (
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='Page' title='Editor' />
+      <RichTextEditorComponent>
+        <EditorData />
+        <Inject services={[Image, Link, Toolbar, HtmlEditor, QuickToolbar]} />
+      </RichTextEditorComponent>
+    </div>
+  );
 };
 
 export default Editor;
