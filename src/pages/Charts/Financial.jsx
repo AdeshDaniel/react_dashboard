b43de46c3a4c1e1af5ab255f029dd2e23dc2efd1
AdeshDaniel <stockhutfxcompany@gmail.com>
2023-03-18 00:36:08 +0100

    finished with the site

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import {
   ChartComponent,
-  SeriesDirective,
   SeriesCollectionDirective,
+  SeriesDirective,
   Inject,
+  HiloSeries,
   Tooltip,
   DateTime,
   Zoom,
   Logarithmic,
   Crosshair,
-  HiloSeries,
 } from "@syncfusion/ej2-react-charts";
+
 import {
   financialChartData,
   FinancialPrimaryXAxis,
@@ -19,14 +20,26 @@ import {
 import { useStateContext } from "../../contexts/ContextProvider";
 import { ChartsHeader } from "../../components";
 
+const date1 = new Date("2017, 1, 1");
+
+// eslint-disable-next-line consistent-return
+function filterValue(value) {
+  if (value.x >= date1) {
+    // eslint-disable-next-line no-sequences
+    return value.x, value.high, value.low;
+  }
+}
+const returnValue = financialChartData.filter(filterValue);
+
 const Financial = () => {
   const { currentMode } = useStateContext();
 
   return (
     <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
-      <ChartsHeader category='Financial' title='Apple Financial' />
+      <ChartsHeader category='Financial' title='AAPLE Historical' />
       <div className='w-full'>
         <ChartComponent
+          id='charts'
           primaryXAxis={FinancialPrimaryXAxis}
           primaryYAxis={FinancialPrimaryYAxis}
           chartArea={{ border: { width: 0 } }}
@@ -34,21 +47,27 @@ const Financial = () => {
           crosshair={{ enable: true, lineType: "Vertical", line: { width: 0 } }}
           background={currentMode === "Dark" ? "#33373E" : "#fff"}
         >
-          <SeriesCollectionDirective>
-            {financialChartData.map((item, index) => (
-              <SeriesDirective key={index} {...item} />
-            ))}
-          </SeriesCollectionDirective>
           <Inject
             services={[
+              HiloSeries,
               Tooltip,
               DateTime,
-              Zoom,
               Logarithmic,
               Crosshair,
-              HiloSeries,
+              Zoom,
             ]}
           />
+          <SeriesCollectionDirective>
+            <SeriesDirective
+              dataSource={returnValue}
+              xName='x'
+              yName='low'
+              name='Apple Inc'
+              type='Hilo'
+              low='low'
+              high='high'
+            />
+          </SeriesCollectionDirective>
         </ChartComponent>
       </div>
     </div>
